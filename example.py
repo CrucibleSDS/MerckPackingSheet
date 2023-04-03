8b4146e980a2a6e694af19bef43aad35f8924c92
Brian Lu <me@greencappuccino.net>
2023-04-03 19:37:11 -0400

    new template

--- example.py
+++ example.py
@@ -1,18 +1,34 @@
-import templater
+import os
+from pathlib import Path
 
-data = {
-	'headers': ["Item", "CAS No.", "Weight", "Mass \\%", "Emergency Telephone"],
-	'signal_word': "Danger",
-	'rows': [
-		["Rubber Ducky", "000000-00", "400 kg", "80\\%", "+1(630) 000-0000"],
-		["Dino", "000001-00", "100 kg", "20\\%", "+1(630) 000-0000"],
-	],
-	'pictograms': ["PICT_GHS04_COMPRESSED_GAS", "PICT_GHS07_HARMFUL"]
-}
+from templater import Templater, PaperType
 
-templater = templater.Templater()
+t = Templater()
 
-output = templater.template_front_page(data)
+cover = t.generate_pdf({
+    'graphicspath': f"{(Path(__file__).parent / 'img').absolute()}{os.sep}",
+    'paper': PaperType.A4_PAPER.value,
+    'cover_title': "Cover Sheet — U.S. Origin Shipments",
+    'sensitivity': "Proprietary",
+    'product_name': "Test Product",
+    'destination_country': "United States",
+    'columns': 'L|l|l|l|l|l',
+    'headers': [
+        "Product Name",
+        "CAS No.",
+        "Product Brand",
+        "Product Number",
+        f"{'Mass'} \\%"
+    ],
+    'signal_word': None,
+    'rows': [
+        ["Rubber Ducky", "000000-00", "400 kg", "80\\%", "+1(630) 000-0000"],
+        ["Dino", "000001-00", "100 kg", "20\\%", "+1(630) 000-0000"],
+    ],
+    'pictograms': ["PICT_GHS04_COMPRESSED_GAS", "PICT_GHS07_HARMFUL"],
+    'hazard_statement_overview': ["EMIT_FLAMMABLE_GASES", "HAZARDOUS_TO_AQUATIC_ENVIRONMENT"],
+    'signature_Date': "September 38, 2023",
+})
 
-with open("example-output.tex", "w") as f:
-	f.write(output)
+with open("example-output.pdf", "wb") as f:
+    f.write(cover.getbuffer())
--- templater.py
+++ templater.py
@@ -1,11 +1,121 @@
+import enum
+import logging
+import os
+import subprocess
+from enum import Enum
+from io import BytesIO
 from pathlib import Path
+from tempfile import TemporaryDirectory
+
 from jinja2 import Environment, FileSystemLoader
+from PyPDF2 import PdfMerger
+
 
 class Templater:
-	def __init__(self):
-		self.file_loader = FileSystemLoader(Path(__file__).parent)
-		self.env = Environment(loader=self.file_loader)
-		self.template = self.env.get_template('template.tex')
+    def __init__(self):
+        self.file_loader = FileSystemLoader(Path(__file__).parent)
+        self.env = Environment(loader=self.file_loader)
+        self.template = self.env.get_template('template.tex')
+        self.logger = logging.getLogger("Templater")
 
-	def template_front_page(self, data: dict[str, any]) -> str:
-		return self.template.render(data=data)
+    def generate_pdf(self, data: dict[str, any]) -> BytesIO:
+        data['graphicspath'] = f"{(Path(__file__).parent / 'img').absolute()}{os.sep}"
+
+        with TemporaryDirectory() as td:
+            args = [
+                'pdflatex',
+                '-interaction-mode=batchmode',
+                f'-output-directory={td}',
+                '-jobname=template',
+                '-shell-escape',
+            ]
+            print(self.template.render(data=data))
+            subprocess.run(args,
+                           input=self.template.render(data=data).encode(encoding='utf-8'),
+                           cwd=td,
+                           timeout=15,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+            with open(Path(td, 'template.log'), 'rb') as f:
+                print(f.read().decode('utf-8'))
+                #self.logger.debug()
+                pass
+            with open(Path(td, 'template.pdf'), 'rb') as f:
+                pdf = f.read()
+        return BytesIO(pdf)
+
+
+class HazardStatementOverview(Enum):
+    ACUTE_TOXICITY = enum.auto()
+    ASPIRATIONAL_HAZARD = enum.auto()
+    CARCINOGENCITY = enum.auto()
+    EMIT_FLAMMABLE_GASES = enum.auto()
+    COMBUSTIBLE_DUST = enum.auto()
+    CORROSIVE_TO_METALS = enum.auto()
+    EXPLOSIVES = enum.auto()
+    FLAMMABLE_AEROSOLS = enum.auto()
+    FLAMMABLE_LIQUIDS = enum.auto()
+    FLAMMABLE_SOLIDS = enum.auto()
+    GASES_UNDER_PRESSURE = enum.auto()
+    GERM_CELL_MUTAGENCITY = enum.auto()
+    HAZARDOUS_TO_AQUATIC_ENVIRONMENT = enum.auto()
+    HAZARDOUS_TO_OZONE_LAYER = enum.auto()
+    OXIDIZING_GASES = enum.auto()
+    OXIDIZING_LIQUIDS = enum.auto()
+    OXIDIZING_SOLIDS = enum.auto()
+    ORGANIC_PEROXIDES = enum.auto()
+    PYROPHORIC_GAS = enum.auto()
+    PYROPHORIC_LIQUIDS = enum.auto()
+    PYROPHORIC_SOLID = enum.auto()
+    REPRODUCTIVE_TOXICITY = enum.auto()
+    RESPIRATORY_OR_SKIN_SENSITIZATION = enum.auto()
+    SELF_HEATING_CHEMICALS = enum.auto()
+    SELF_REACTIVE_CHEMICALS = enum.auto()
+    SERIOUS_EYE_DAMAGE_IRRITATION = enum.auto()
+    SKIN_CORROSION_IRRITATION = enum.auto()
+    SIMPLE_ASPHYXIANT = enum.auto()
+    ORGAN_TOXICITY_REPEATED_PROLONGED = enum.auto()
+    ORGAN_TOXICITY_SINGLE = enum.auto()
+    OTHER = enum.auto()
+    NOT_APPLICABLE = enum.auto()
+
+
+class PaperType(Enum):
+    A4_PAPER = 'a4paper'
+    A5_PAPER = 'a5paper'
+    B5_PAPER = 'b5paper'
+    LETTER_PAPER = 'letterpaper'
+    LEGAL_PAPER = 'legalpaper'
+
+
+class SensitivityLabels(Enum):
+    PROPRIETARY = 'Proprietary'
+    CONFIDENTIAL = 'Confidential'
+    SENSITIvE = 'Sensitive'
+    PUBLIC = 'Public'
+
+
+def merge_pdf(pdfs: list[BytesIO]) -> BytesIO:
+    bytesio = BytesIO()
+    merger = PdfMerger()
+    for pdf in pdfs:
+        merger.append(pdf)
+    merger.write(bytesio)
+    merger.close()
+    return bytesio
+
+
+def fix_si(amount):
+    prefixes = [
+        ('T', 1e12),
+        ('G', 1e9),
+        ('M', 1e6),
+        ('k', 1e3),
+        ('', 1),
+        ('m', 1e-3),
+        ('µ', 1e-6),
+        ('n', 1e-9)]
+    for prefix, factor in prefixes:
+        if amount >= factor:
+            return f'{amount / factor:.2f}{prefix}g'
+    return f'{amount}g'
